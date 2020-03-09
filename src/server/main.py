929ca74aa5fcd62295a925d98476a07eea948d76
Daniel Hnyk <hnykda@gmail.com>
2020-03-09 13:53:50 +0100

    blackify

--- src/server/main.py
+++ src/server/main.py
@@ -1,9 +1,9 @@
 import os
 
+from bokeh.embed import server_document
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from bokeh.embed import server_document
 
 # the URI must be accessible from the client's browser, it's not "proxied" via the server
 BOKEH_URI = os.getenv("BOKEH_URI", "http://0.0.0.0:5001")
@@ -19,5 +19,6 @@ async def index(request: Request) -> Response:
     plot_script_1 = server_document(os.path.join(BOKEH_URI, "app1"))
     plot_script_2 = server_document(os.path.join(BOKEH_URI, "app2"))
     return templates.TemplateResponse(
-        "index.html", {"request": request, "plot_1": plot_script_1, "plot_2": plot_script_2}
+        "index.html",
+        {"request": request, "plot_1": plot_script_1, "plot_2": plot_script_2},
     )
