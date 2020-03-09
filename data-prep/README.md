49f61cf4d591fd6a9daa55868fed47317e7d66ed
Daniel Hnyk <hnykda@gmail.com>
2020-03-09 09:16:39 +0100

    done

---
+++ data-prep/README.md
@@ -0,0 +1,22 @@
+## Fixing gleamviz data
+First fix `md_*.tsv` files which sometime countain two records of a single line. The script
+also removes unnecessary `ID` column to unify the format with the rest of the exported files:
+
+```
+python remove_doubles.py <gleamviz-exported-data> data_fixed
+```
+
+and then remove leading space in all TSV files:
+
+```
+sed -i 's/^\t/  /; s/  //' **.tsv
+```
+
+then you should be able to standardly load the data in pandas
+
+```python
+import pandas as pd
+filename = "md_countries.tsv"  # or any other file
+df = pd.read_csv(filename, sep="\t", index_col=0)
+```
+
---
+++ data-prep/remove_doubles.py
@@ -0,0 +1,32 @@
+import glob
+import argparse
+import os
+import shutil
+import re
+
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", help="Directory with the gleamviz exported data")
+parser.add_argument("outdir", help="Output directory")
+args = parser.parse_args()
+
+shutil.copytree(args.directory, args.outdir)
+os.chdir(args.outdir)
+
+mds = glob.glob("md_*")
+
+for md in mds:
+    s = ""
+    with open(md) as ifile:
+        next(ifile)
+        s += '#\t"Name"\n'
+        for line in ifile:
+            m = re.findall(r"[0-9]+", line)
+            if len(m) == 2:
+                ix = line.index(m[1])
+                first = line[:ix].rstrip("\t")
+                second = line[ix:]
+                s += first + "\n\t" + second
+            else:
+                s += line
+    with open(md, "w") as ofile:
+        ofile.write(s)
