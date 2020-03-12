798165381ea3f64962f74853b23041d6fc21b5c8
Daniel Hnyk <hnykda@gmail.com>
2020-03-12 18:02:05 +0100

    fix tests

--- tests/server/test_main.py
+++ tests/server/test_main.py
@@ -9,11 +9,14 @@ def test_index():
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_selections():
     response = client.get("/selections")
     assert response.status_code == 200
 
 
 def test_result_calculations():
-    response = client.get("/result-calculations", params={"number": 10, "datepicker": "2019/12/22"})
+    response = client.get(
+        "/result-calculations", params={"number": 10, "datepicker": "2019/12/22"}
+    )
     assert response.status_code == 200
