5a4763fda254e0a8c16967d55ac846327deeb054
Jan Pipek <jan.pipek@gmail.com>
2020-03-14 14:25:04 +0100

    World bubble map POC (#17)
    * World bubble map POC
    
    * Adapt to bokeh server
    
    * Show selected cities
    
    * Simple link POC
    
    Aesthetically not very pleasing though
    
    * A bit of UI
    
    * black the world_map
    
    * Black world map
    
    * Fix: map URL in server
    
    * Click-through in the world map
    
    * Black the black map
    
    * Correct /model URL for the links
    
    * adding the world_map to the deployment (#45)
    
    Co-authored-by: Daniel Hnyk <hnykda@users.noreply.github.com>

--- src/server/main.py
+++ src/server/main.py
@@ -21,7 +21,7 @@ templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, "templates"))
 async def index(request: Request) -> Response:
     """TODO: this is the main hompage, should have a bubble map which should
     link ot the /model"""
-    bubble_map_script = server_document(f"{BOKEH_URI}/app2")
+    bubble_map_script = server_document(f"{BOKEH_URI}/world_map")
     return templates.TemplateResponse(
         "index.html", {"request": request, "plot": bubble_map_script},
     )
