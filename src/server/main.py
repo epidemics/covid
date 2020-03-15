90bcb168f9e01c85f2ca7cf09b0a9b8f7556fd84
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-03-15 19:06:55 +0100

    Replace long list by for loop as it will be used in many places

--- src/server/main.py
+++ src/server/main.py
@@ -1,9 +1,48 @@
 import os
 
+from datetime import date
+import numpy as np
+import pandas as pd
+
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+
+PLACES = [
+    "Africa",
+    "Australia",
+    "Belgium",
+    "California",
+    "China",
+    "Dubai",
+    "Egypt",
+    "European Union",
+    "France",
+    "Germany",
+    "Hong Kong",
+    "Hubei",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Italy",
+    "Japan",
+    "London",
+    "Netherlands",
+    "New York",
+    "Oxford",
+    "Russia",
+    "San Francisco",
+    "Singapore",
+    "South Korea",
+    "Spain",
+    "Switzerland",
+    "The Middle East",
+    "United Kingdon",
+    "United States of America",
+    "Washington",
+    "Wuhan"
+]
 
 SERVER_ROOT = os.path.dirname(__file__)
 
@@ -13,6 +52,11 @@ app.mount(
 )
 
 templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, "templates"))
+
+
+class Place:
+    def __init__(self, name):
+        self.name = name
 
 
 @app.get("/")
@@ -26,8 +70,12 @@ async def index(request: Request) -> Response:
 async def request_calculation(request: Request) -> Response:
     """TODO: This view should process a form"""
     """MATI: Actually, I don't think we'll have anything to process here as the first page just has a START button"""
-    return templates.TemplateResponse("request-calculation.html", {"request": request},)
 
+    # TODO: Use real data model here
+
+    places = [Place(place) for place in PLACES]
+
+    return templates.TemplateResponse("request-calculation.html", {"request": request, "message": "Please provide data", "places": places},)
 
 @app.get("/model")
 async def model(request: Request, city: str = "New York") -> Response:
@@ -39,9 +87,12 @@ async def model(request: Request, city: str = "New York") -> Response:
 
 @app.get("/request-event-evaluation")
 async def request_event_evaluation(request: Request) -> Response:
+
+    places = [Place(place) for place in PLACES]
+
     return templates.TemplateResponse(
         "request-event-evaluation.html",
-        {"request": request, "message": "Please provide data"},
+        {"request": request, "message": "Please provide data", "places": places},
     )
 
 @app.get("/result-calculations")
@@ -51,9 +102,6 @@ async def result_calculations(
     # TODO: Use real data model here
     class Place:
         def __init__(self, name):
-            from datetime import date
-            import numpy as np
-            import pandas as pd
             self.name = name
             self.population = 4000000
             self.gleamviz_predictions = pd.Series(  # From gleamviz
