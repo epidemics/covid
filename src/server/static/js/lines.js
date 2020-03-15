a712177ae85aae42a25c700eb1166e49a90be633
Matej Vrzala <vrzala.matej@gmail.com>
2020-03-15 12:54:24 +0100

    add ticks, reformat

--- src/server/static/js/lines.js
+++ src/server/static/js/lines.js
@@ -1,4 +1,5 @@
-
+Date.prototype.addDays=function(d){return new Date(this.valueOf()+864E5*d);};
+let today = new Date();
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 100, bottom: 30, left: 30},
     width = 460 - margin.left - margin.right,
@@ -6,10 +7,17 @@ var margin = {top: 10, right: 100, bottom: 30, left: 30},
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+   .append("svg")
+
+    .attr("width", '100%')
+    .attr("height", '100%')
+    // .attr('viewBox','0 0 '+Math.min(width,height)+' '+Math.min(width,height))
+    // .attr('preserveAspectRatio','xMinYMin')
+    .append("g") 
+  // .append("svg")
+  //   .attr("width", width + margin.left + margin.right)
+  //   .attr("height", height + margin.top + margin.bottom)
+  // .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
@@ -24,7 +32,7 @@ function transformData(data) {
       result[row.City] = []
     }
     result[row.City].push([
-          row.Timestep, 
+          today.addDays(row.Timestep), 
           row['Cumulative Median_0'], 
           row['Cumulative Median_1'], 
           row['Cumulative Median_2'],
@@ -37,11 +45,12 @@ function transformData(data) {
 //Read the data
 d3.csv("https://csvlint.io/validation/5e6cf73ea8838f0004000004.csv")
   .then(function(data){
-    console.log('data', data)
+    console.log('raw data', data)
     // List of groups (here I have one group per column)
     var allGroup = getCountries(data)
     data = transformData(data)
-    console.log('dataT', data)
+    countryData = data['Abuja']
+    console.log('transformed data', data)
 
     // add the options to the button
     d3.select("#selectButton")
@@ -52,127 +61,75 @@ d3.csv("https://csvlint.io/validation/5e6cf73ea8838f0004000004.csv")
       .text(function (d) { return d; }) // text showed in the menu
       .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-    // // A color scale: one color for each group
-    // var myColor = d3.scaleOrdinal()
-    //   .domain(allGroup)
-    //   .range(d3.schemeSet2);
 
     // Add X axis --> it is a date format
-    var x = d3.scaleLinear()
-      .domain([0,365])
-      .range([ 0, width ]);
+    var x = d3.scaleTime()
+      .range([ 0, width ])
+            // .domain( [0,1000])
+      .domain(d3.extent(countryData, function(d) { 
+        console.log('x called', d);
+        return d[0]; 
+      }))
+      // .ticks(d3.time.months, 1).tickFormat(d3.time.format("%b"));;
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(d3.axisBottom(x).ticks(6).tickFormat(d3.timeFormat("%Y-%m-%d")));
+
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain( [0,1000])
+      .domain([0,1000])
+      //.domain([0, d3.max(countryData, function(d) { return Math.max(+d[1], +d[2], +d[3], +d[4]); })])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
 
-    // Initialize line with group a
-    var line = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(+d[0]) })
-          .y(function(d) { return y(+d[1]) })
-        )
-        .attr("stroke", 'blue')
-        .style("stroke-width", 1)
-        .style("fill", "none")
+    function drawLine(i, color) {
+      return svg
+        .append('g')
+        .append("path")
+          .datum(countryData)
+          .attr("d", d3.line()
+            .x(function(d) { console.log('asdsa'); window.myX = x; return x(d[0]) })
+            .y(function(d) { return y(+d[i]) })
+          )
+          .attr("stroke", color)
+          .style("stroke-width", 1)
+          .style("fill", "none")
+    }
 
-    // Initialize line with group a
-    var line2 = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(+d[0]) })
-          .y(function(d) { return y(+d[2]) })
-        )
-        .attr("stroke", 'red')
-        .style("stroke-width", 1)
-        .style("fill", "none")
+    var line1 = drawLine(1, 'blue')
+    var line2 = drawLine(2, 'red')
+    var line3 = drawLine(3, 'orange')
+    var line4 = drawLine(4, 'green')
 
-    // Initialize line with group a
-    var line3 = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(+d[0]) })
-          .y(function(d) { return y(+d[3]) })
-        )
-        .attr("stroke", 'orange')
-        .style("stroke-width", 1)
-        .style("fill", "none")
-
-    // Initialize line with group a
-    var line4 = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(+d[0]) })
-          .y(function(d) { return y(+d[4]) })
-        )
-        .attr("stroke", 'green')
-        .style("stroke-width", 1)
-        .style("fill", "none")
-
-      console.log(data, data.keys())
+    console.log('draw line was called')
+  
     update('Abuja')
 
     // A function that update the chart
     function update(selectedGroup) {
-      // Create new data with the selection?
-      var dataFilter = data[selectedGroup]
-      console.log('update', dataFilter)
+      countryData = data[selectedGroup]
+      console.log('countryData',countryData)
+      function updateLine(myLine, i){
 
-      // Give these new data to update line
-      line
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d[0]) })
-            .y(function(d) { return y(+d[1]) })
-          )
-          .attr("stroke", 'blue')
+          myLine
+            .datum(countryData)
+            .transition()
+            .duration(1000)
+            .attr("d", d3.line()
+              .x(function(d) { return x(d[0]) })
+              .y(function(d) { return y(+d[i]) })
+            )
 
-      line2
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d[0]) })
-            .y(function(d) { return y(+d[2]) })
-          )
-          .attr("stroke", 'red')
 
-      line3
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d[0]) })
-            .y(function(d) { return y(+d[3]) })
-          )
-          .attr("stroke", 'orange')
+      }
 
-      line4
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d[0]) })
-            .y(function(d) { return y(+d[4]) })
-          )
-          .attr("stroke", 'green')
+      updateLine(line1, 1)
+      updateLine(line2, 2)
+      updateLine(line3, 3)
+      updateLine(line4, 4)
+
     }
 
     // When the button is changed, run the updateChart function
