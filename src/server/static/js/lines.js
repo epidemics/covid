88bba5be869cea8c521ec20b5e5f280aea527cc4
Matej Vrzala <vrzala.matej@gmail.com>
2020-03-17 22:33:49 +0100

    change graph heigh again

--- src/server/static/js/lines.js
+++ src/server/static/js/lines.js
@@ -23,7 +23,7 @@ var chartDiv = document.getElementById("my_dataviz"); //Mati: this doesn't seem 
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 100, bottom: 30, left: 100 },
   width = 600,
-  height = 690;
+  height = 675;
 // append the svg object to the body of the page
 var graphSize = "75%";
 var svg = d3
