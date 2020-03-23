ac1cb8cb6e2fe898f76804f7346f00e5615212c5
Vojtech Vesely <vesely.v4@gmail.com>
2020-03-23 17:31:50 +0100

    data added

--- src/server/static/js/case-map.js
+++ src/server/static/js/case-map.js
@@ -1,7 +1,14 @@
-Plotly.d3.csv('/static/data/test_data.csv', function(err, rows) {
+Plotly.d3.csv('https://storage.googleapis.com/static-covid/static/case-map-data.csv', function(err, rows) {
 	function unpack(rows, key) {
 		return rows.map(function(row) {
 			return row[key];
+		});
+	}
+
+
+	function get_text(rows) {
+		return rows.map(function(row) {
+			return row["est_active"] + " estimated active";
 		});
 	}
 
@@ -10,10 +17,10 @@ Plotly.d3.csv('/static/data/test_data.csv', function(err, rows) {
 		name: "US states",
 		geojson: "/static/data/custom.geo.json",
 		featureidkey: "properties.iso_a3",
-		locations: unpack(rows, 'CODE'),
-        z: unpack(rows, 'GDP (BILLIONS)'),
-        text: unpack(rows, 'COUNTRY'),
-		colorscale: [[0.0,'rgb(255,255,255)'],[1,'rgb(255,0,0)']],
+		locations: unpack(rows, 'code'),
+        z: unpack(rows, 'risk'),
+        text: get_text(rows),
+		colorscale: [[0,'rgb(255,255,255)'],[1,'rgb(255,0,0)']],
 		showscale: true,
 		colorbar: {
 			y: 0,
