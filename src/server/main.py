1e2181ccab83e5a0ad81d82b5a5d1caebcfc40b6
Jan Pipek <jan.pipek@gmail.com>
2020-03-25 08:45:21 +0100

    Delete anything unused from main.py

--- src/server/main.py
+++ src/server/main.py
@@ -1,54 +1,11 @@
 import os
 
-import pandas as pd
-
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from server.event_model import stress, excess
-
-# from server.notion_connect import query_containment_measures
 from server.config import CONFIG
 
-PLACES = [
-    "Africa",
-    "Australia",
-    "Belgium",
-    "California",
-    "China",
-    "Dubai",
-    "Egypt",
-    "European Union",
-    "France",
-    "Germany",
-    "Hong Kong",
-    "Hubei",
-    "India",
-    "Indonesia",
-    "Iran",
-    "Italy",
-    "Japan",
-    "London",
-    "Netherlands",
-    "New York",
-    "Oxford",
-    "Russia",
-    "San Francisco",
-    "Singapore",
-    "South Korea",
-    "Spain",
-    "Switzerland",
-    "The Middle East",
-    "United Kingdon",
-    "United States of America",
-    "Washington",
-    "Wuhan",
-]
-
-CAPITALS = pd.read_csv(
-    "https://raw.githubusercontent.com/icyrockcom/country-capitals/master/data/country-list.csv"
-)
 
 SERVER_ROOT = os.path.dirname(__file__)
 
@@ -58,50 +15,6 @@ app.mount(
 )
 
 templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, "templates"))
-
-
-# TODO: for testing purposes, make a fixture/mock
-if CONFIG.ENABLE_NOTION:
-    CONTAINMENT_MEAS = query_containment_measures()
-else:
-    CONTAINMENT_MEAS = None
-
-LINES = pd.read_csv(
-    "https://storage.googleapis.com/static-covid/static/line-data-v2.csv?cache_bump=2"
-)
-
-STARTDATE = pd.to_datetime("03/14/2020", format="%m/%d/%Y")
-
-# TODO: Does it break anything to replace PLACES above?
-
-PLACES_GV = {
-    "Egypt": 9.7e7,
-    "China": 1.4e9,
-    "Hong Kong": 7.4e6,
-    "India": 1.3e9,
-    "Russia": 1.44e8,
-    "South Korea": 5.1e7,
-    "Singapore": 5.6e6,
-    "Japan": 1.28e8,
-    "Indonesia": 2.64e8,
-    "Dubai": 3.3e6,
-    "Iran": 8.1e7,
-    "US": 3.3e8,
-    "Australia": 2.5e7,
-    "France": 6.7e7,
-    "Italy": 6e7,
-    "Germany": 8.3e7,
-    "Spain": 4.7e7,
-    "Netherlands": 1.7e7,
-    "Switzerland": 8.5e6,
-    "United Kingdom": 6.6e7,
-    "Czech Republic": 1.1e7,
-}
-
-
-class Place:
-    def __init__(self, name):
-        self.name = name
 
 
 @app.get("/")
@@ -118,13 +31,9 @@ async def case_map(request: Request) -> Response:
 @app.get("/request-calculation")
 async def request_calculation(request: Request) -> Response:
     """TODO: This view should process a form"""
-    # TODO: Use real data model here
-
-    places = [Place(place) for place in PLACES]
-
     return templates.TemplateResponse(
         "request-calculation.html",
-        {"request": request, "message": "Please provide data", "places": places},
+        {"request": request, "message": "Please provide data"},
     )
 
 
