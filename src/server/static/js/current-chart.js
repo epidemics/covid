2c91c850340d507daf0cfbb865c98e65d6662de1
Mathijs Henquet <mathijs.henquet@gmail.com>
2020-03-29 12:17:33 +0200

    added confirmed

--- src/server/static/js/current-chart.js
+++ src/server/static/js/current-chart.js
@@ -29,7 +29,7 @@ var layout = {
     tickcolor: "#fff"
   },
   yaxis: {
-    title: "Active infections (% of population)",
+    title: "Active infections",
     titlefont: {
       family: "DM Sans, sans-serif",
       size: 18,
@@ -101,7 +101,7 @@ function applyVariance(mean, vars, sigma){
 
 function addHistoricalData(plotlyGraph, pop){
   let cfr = 0.016;
-  let cfr_log_cv = 0.5; // coefficent of variance (relative sd) of cfr
+  let cfr_cv = 0.5; // coefficent of variance (relative sd) of cfr
 
   let incubation_period = 5;
   let onset_to_death = 10;
@@ -116,7 +116,7 @@ function addHistoricalData(plotlyGraph, pop){
 
     // error bars
     let errors = {
-      y: deaths.map((v) => applyVariance(v/cfr/pop, [cfr_log_cv, 3/Math.sqrt(v)], 1)),
+      y: deaths.map((v) => applyVariance(v/cfr/pop, [cfr_cv, 3/Math.sqrt(v)], 1)),
       x: dates,
       mode: "lines",
       line: {color: "transparent"},
@@ -125,6 +125,7 @@ function addHistoricalData(plotlyGraph, pop){
       type: "scatter",
       name: "Estimated error",
       showlegend: false,
+      hoverinfo: 'skip'
     }
 
     let max = errors.y[errors.y.length - 1];
@@ -134,26 +135,37 @@ function addHistoricalData(plotlyGraph, pop){
 
     for(let i = deaths.length-1; i >= 0; i--){
       let v = deaths[i];
-      errors.y.push(applyVariance(v/cfr/pop, [1/2, 10/Math.sqrt(v)],-1));
+      errors.y.push(applyVariance(v/cfr/pop, [cfr_cv, 10/Math.sqrt(v)],-1));
       errors.x.push(dates[i]);
     }
 
+    var f = d3.format(".2s");
+    let text = deaths.map((v, i) => {
+      let low = applyVariance(v/cfr/pop, [1/2, 10/Math.sqrt(1+v)],-1);
+      let high = applyVariance(v/cfr/pop, [1/2, 10/Math.sqrt(1+v)],1);
+      return `Estimated: ${f(low)}-${f(high)}`;
+    });
+
     let data = [
-      // {
-      //   mode: "markers",
-      //   x: dates,
-      //   y: confirmed,
-      //   line: {color: "#fff"},
-      //   type: "scatter",
-      //   name: "Confirmed"
-      // },
+      {
+        mode: "markers",
+        x: dates,
+        y: confirmed,
+        line: {color: "#fff"},
+        type: "scatter",
+        name: "Confirmed",
+        marker: {size: 3},
+        hovertemplate: 'Confirmed: %{y:.3s}',
+      },
       {
         mode: "lines",
         x: dates,
         y: deaths.map((death) => death/cfr/pop),
         line: {color: "#fff"},
         type: "scatter",
-        name: "Estimated"
+        name: "Estimated",
+        hoverinfo: 'text',
+        text: text
       },
       errors
     ];
