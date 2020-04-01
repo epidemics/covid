ef23f490cfba9f082e6c9abd555f8ccac710dbf6
Michal Ježek <michal.jezek@moneta.cz>
2020-04-01 19:00:42 +0200

    Reformated by black

--- src/server/main.py
+++ src/server/main.py
@@ -15,8 +15,7 @@ SERVER_ROOT = os.path.dirname(__file__)
 
 app = FastAPI()
 app.mount(
-    "/static", StaticFiles(directory=os.path.join(SERVER_ROOT, "static")),
-    name="static"
+    "/static", StaticFiles(directory=os.path.join(SERVER_ROOT, "static")), name="static"
 )
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
@@ -27,16 +26,14 @@ templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, "templates"))
 async def model(request: Request) -> Response:
     """serve the main model visualization"""
     return templates.TemplateResponse(
-        "model.html",
-        {"request": request, "STATIC_URL": STATIC_URL}
+        "model.html", {"request": request, "STATIC_URL": STATIC_URL}
     )
 
 
 @app.get("/case-map")
 async def case_map(request: Request) -> Response:
     return templates.TemplateResponse(
-        "case-map.html",
-        {"request": request, "STATIC_URL": STATIC_URL},
+        "case-map.html", {"request": request, "STATIC_URL": STATIC_URL},
     )
 
 
@@ -44,17 +41,18 @@ async def case_map(request: Request) -> Response:
 async def request_calculation(request: Request) -> Response:
     return templates.TemplateResponse(
         "request-calculation.html",
-        {"request": request,
-         "message": "Please provide data",
-         "STATIC_URL": STATIC_URL},
+        {
+            "request": request,
+            "message": "Please provide data",
+            "STATIC_URL": STATIC_URL,
+        },
     )
 
 
 @app.get("/about")
 async def about(request: Request) -> Response:
     return templates.TemplateResponse(
-        "about.html",
-        {"request": request, "STATIC_URL": STATIC_URL},
+        "about.html", {"request": request, "STATIC_URL": STATIC_URL},
     )
 
 
@@ -72,6 +70,5 @@ async def containment_with_dot():
 @app.get("/containment")
 async def containment(request: Request) -> Response:
     return templates.TemplateResponse(
-        "containment.html",
-        {"request": request, "STATIC_URL": STATIC_URL},
+        "containment.html", {"request": request, "STATIC_URL": STATIC_URL},
     )
