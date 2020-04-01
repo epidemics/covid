c00495ece3fea21f60cef4c8cc5594a2e0beb49f
Michal Ježek <michal.jezek@moneta.cz>
2020-04-01 18:55:59 +0200

    Reformated to pass the checks

--- src/server/main.py
+++ src/server/main.py
@@ -10,11 +10,13 @@ from server.config import CONFIG
 
 
 STATIC_URL = "https://storage.googleapis.com/static-covid/static/staging"
+# STATIC_URL = "https://storage.googleapis.com/static-covid/static/production"
 SERVER_ROOT = os.path.dirname(__file__)
 
 app = FastAPI()
 app.mount(
-    "/static", StaticFiles(directory=os.path.join(SERVER_ROOT, "static")), name="static"
+    "/static", StaticFiles(directory=os.path.join(SERVER_ROOT, "static")),
+    name="static"
 )
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
@@ -24,25 +26,36 @@ templates = Jinja2Templates(directory=os.path.join(SERVER_ROOT, "templates"))
 @app.get("/")
 async def model(request: Request) -> Response:
     """serve the main model visualization"""
-    return templates.TemplateResponse("model.html", {"request": request, "STATIC_URL": STATIC_URL})
+    return templates.TemplateResponse(
+        "model.html",
+        {"request": request, "STATIC_URL": STATIC_URL}
+    )
 
 
 @app.get("/case-map")
 async def case_map(request: Request) -> Response:
-    return templates.TemplateResponse("case-map.html", {"request": request, "STATIC_URL": STATIC_URL},)
+    return templates.TemplateResponse(
+        "case-map.html",
+        {"request": request, "STATIC_URL": STATIC_URL},
+    )
 
 
 @app.get("/request-calculation")
 async def request_calculation(request: Request) -> Response:
     return templates.TemplateResponse(
         "request-calculation.html",
-        {"request": request, "message": "Please provide data", "STATIC_URL": STATIC_URL},
+        {"request": request,
+         "message": "Please provide data",
+         "STATIC_URL": STATIC_URL},
     )
 
 
 @app.get("/about")
 async def about(request: Request) -> Response:
-    return templates.TemplateResponse("about.html", {"request": request, "STATIC_URL": STATIC_URL},)
+    return templates.TemplateResponse(
+        "about.html",
+        {"request": request, "STATIC_URL": STATIC_URL},
+    )
 
 
 @app.get("/status")
@@ -58,4 +71,7 @@ async def containment_with_dot():
 
 @app.get("/containment")
 async def containment(request: Request) -> Response:
-    return templates.TemplateResponse("containment.html", {"request": request, "STATIC_URL": STATIC_URL},)
+    return templates.TemplateResponse(
+        "containment.html",
+        {"request": request, "STATIC_URL": STATIC_URL},
+    )
