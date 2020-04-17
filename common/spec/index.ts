f18dabd54bb6839e51d1b809ce9850238b151a95
Mathijs Henquet <mathijs.henquet@gmail.com>
2020-04-17 18:52:26 +0200

    Ensure we can only import the whole ns

--- common/spec/index.ts
+++ common/spec/index.ts
@@ -1,3 +1,2 @@
-
-export * as v4 from "./v4";
-export * as v3 from "./v3";
+export { v4 } from "./v4";
+export { v3 } from "./v3";
--- common/spec/v3.ts
+++ common/spec/v3.ts
@@ -1,19 +1,21 @@
 import * as Plotly from "plotly.js";
 
-export interface ModelTrace {
-  hoverinfo: string;
-  line: Partial<Plotly.ScatterLine>;
-  hoverlabel: { namelength: -1 };
-  opacity: number;
-  name?: string;
-  showlegend: boolean;
-  x: Array<string>;
-  y: Array<number>;
-  type: "scatter";
-}
+export namespace v3 {
+  export interface ModelTrace {
+    hoverinfo: string;
+    line: Partial<Plotly.ScatterLine>;
+    hoverlabel: { namelength: -1 };
+    opacity: number;
+    name?: string;
+    showlegend: boolean;
+    x: Array<string>;
+    y: Array<number>;
+    type: "scatter";
+  }
 
-export type ExternalData = ModelTraces;
+  export type ExternalData = ModelTraces;
 
-export interface ModelTraces {
-  [mitigation: string]: Array<ModelTrace>;
+  export interface ModelTraces {
+    [mitigation: string]: Array<ModelTrace>;
+  }
 }
