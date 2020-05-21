300f7cb71d081ebbbde2e3804d1e4960192c9291
Tomas Witzany <tomas.witzany@blindspot.ai>
2020-05-21 10:18:10 +0200

    forgotten commented out clone

--- scripts/scenarios_urls/generate.sh
+++ scripts/scenarios_urls/generate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -xe
-# git clone https://github.com/neherlab/covid19_scenarios.git
+git clone https://github.com/neherlab/covid19_scenarios.git
 # last confirmed working revision
 # git reset --hard 4accce6575a5f7ff269f53644d8b0956e17be4d2
 
