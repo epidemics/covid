d27b67eba605f06c6244c8d2837b6c778faedc35
Maksym Balatsko <maksym.balatsko@blindspot.ai>
2020-05-27 14:56:51 +0200

    high/low compliment functions calculation

--- frontend/page_mitigation/measures.ts
+++ frontend/page_mitigation/measures.ts
@@ -74,3 +74,31 @@ export const measures: Array<Measure | MeasureGroup> = [
     implies: [{ key: 2, value: 3 }, { key: 3, value: 2 }, { key: 4 }],
   },
 ];
+
+export function calculateHighCompliment(measures: Array<Measure | MeasureGroup>): Array<Measure | MeasureGroup> {
+  let highComplimentMeasures = measures;
+  highComplimentMeasures.forEach(function (measure: Measure | MeasureGroup) {
+    if ("items" in measure) {
+      measure.items.forEach(function (item: Measure) {
+        item.mean = item.mean * 0.9
+      })
+    } else {
+      measure.mean = measure.mean * 0.9
+    }
+  })
+  return highComplimentMeasures;
+}
+
+export function calculateLowCompliment(measures: Array<Measure | MeasureGroup>): Array<Measure | MeasureGroup> {
+  let lowComplimentMeasures = measures;
+  lowComplimentMeasures.forEach(function (measure: Measure | MeasureGroup) {
+    if ("items" in measure) {
+      measure.items.forEach(function (item: Measure) {
+        item.mean = 1 - (1 - item.mean) * 0.9
+      })
+    } else {
+      measure.mean = 1 - (1 - measure.mean) * 0.9
+    }
+  })
+  return lowComplimentMeasures;
+}
