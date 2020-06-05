ce1fdde1b163cefca5620feae1be937b41217e37
Tomas Witzany <tomas.witzany@blindspot.ai>
2020-06-05 16:28:22 +0200

    move dependencies into a cache

--- data-pipeline/scripts/estimate_R.R
+++ data-pipeline/scripts/estimate_R.R
@@ -1,4 +1,5 @@
-requiredPackages <- c('tidyverse','EpiEstim','progress', 'doParallel', 'foreach')
+requiredPackages <- c('tidyverse','EpiEstim', 'doParallel', 'foreach')
+chooseCRANmirror(ind=1, graphics=FALSE)
 for(p in requiredPackages){
   if(!require(p,character.only = TRUE)) install.packages(p)
   library(p,character.only = TRUE)
