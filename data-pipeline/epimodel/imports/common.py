1985199f9997f4beb0a3d07215634bbdd04a1e91
Pavel Janata <pavel@janata.me>
2020-07-15 15:37:25 +0200

    fixed the extension of data, removed the cancled columns from intervention json

---
+++ data-pipeline/epimodel/imports/common.py
@@ -0,0 +1,16 @@
+INTERVENTION_TYPES = [
+    "Mask Wearing",
+    "Symptomatic Testing",
+    "Gatherings <1000",
+    "Gatherings <100",
+    "Gatherings <10",
+    "Some Businesses Suspended",
+    "Most Businesses Suspended",
+    "School Closure",
+    "Stay Home Order",
+    "Travel Screen/Quarantine",
+    "Travel Bans",
+    "Public Transport Limited",
+    "Internal Movement Limited",
+    "Public Information Campaigns",
+]
--- data-pipeline/epimodel/imports/interventions.py
+++ data-pipeline/epimodel/imports/interventions.py
@@ -1,51 +1,14 @@
 import pandas as pd
-
-
-INTERVENTION_TYPES = [
-    "Mask Wearing",
-    "Symptomatic Testing",
-    "Gatherings <1000",
-    "Gatherings <100",
-    "Gatherings <10",
-    "Some Businesses Suspended",
-    "Most Businesses Suspended",
-    "School Closure",
-    "Stay Home Order",
-    "Travel Screen/Quarantine",
-    "Travel Bans",
-    "Public Transport Limited",
-    "Internal Movement Limited",
-    "Public Information Campaigns",
-]
+from .common import INTERVENTION_TYPES
 
 
 def create_intervetions_dict(json_path: str):
     df = pd.read_csv(json_path)
     df = df[["Country Code", "Date"] + INTERVENTION_TYPES]
-
-    canceled_interventions = []
-    for intervention_type in INTERVENTION_TYPES:
-        canceled_name = f"CANCELED {intervention_type}"
-        canceled_interventions.append(canceled_name)
-        df[canceled_name] = df.groupby("Country Code")[intervention_type].transform(
-            invert_col
-        )
-
-    df["interventions"] = df[INTERVENTION_TYPES + canceled_interventions].apply(
+    df["interventions"] = df[INTERVENTION_TYPES].apply(
         lambda x: set(x.keys()[x.values.astype(bool)]), axis=1
     )
     return df.groupby("Country Code").apply(process_country).to_dict()["Interventions"]
-
-
-def invert_col(country_intervention):
-    on_idx = country_intervention[(country_intervention == 1.0)]
-    first_time_on = (
-        on_idx.idxmax() if on_idx.count() > 0 else country_intervention.index.max()
-    )
-    cancled = (~country_intervention.astype(bool)).astype(float)
-    cancled[cancled.index <= first_time_on] = 0.0
-
-    return cancled
 
 
 def process_country(country_df):
