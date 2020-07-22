9d8159eeef1a0e40a8fd083d972d3e1a98f6448d
Pavel Janata <pavel@janata.me>
2020-07-22 11:07:36 +0200

    short-term model improvements * extrapolation

--- data-pipeline/epimodel/pymc3_models/cm_effect/pipeline.py
+++ data-pipeline/epimodel/pymc3_models/cm_effect/pipeline.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import pymc3 as pm
-import pickle
 
 from epimodel.pymc3_models.cm_effect.datapreprocessor import DataPreprocessor
 from epimodel.pymc3_models.cm_effect import CMCombinedModel
@@ -8,23 +7,17 @@ from epimodel.pymc3_models.cm_effect import CMCombinedModel
 
 def run_model(data_file: str, output_file: str, extrapolation_period: int):
     dp = DataPreprocessor()
-    data = dp.preprocess_data(data_file)
+    data = dp.preprocess_data(data_file, extrapolation_period)
 
     with CMCombinedModel(data) as model:
         model.build_model()
 
     with model.model:
-        model.trace = pm.sample(1000, chains=2, cores=6, target_accept=0.95, init="adapt_diag")
-
-    with open('model.pckl', 'wb') as f:
-        pickle.dump(model, f)
-
-    # with open('model.pckl', 'rb') as f:
-    #     model = pickle.load(f)
+        model.trace = pm.sample(1000, chains=2, cores=2, target_accept=0.95)
 
     results = []
     for region in model.data.Rs:
-        region_results = model.region_forward_pass(region, extrapolation_period=extrapolation_period)
+        region_results = model.region_forward_pass(region)
         region_results["Code"] = region
         results.append(region_results)
     result = pd.concat(results, axis="index")
