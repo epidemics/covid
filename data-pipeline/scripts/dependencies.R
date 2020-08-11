9c7cb6f285532a5b297ed49faa491da5e4308c3f
Pavel Janata <pavel@janata.me>
2020-08-11 09:20:59 +0200

    downgrade cpp11 * based on https://github.com/tidyverse/tidyr/issues/1024 * after https://github.com/tidyverse/tidyr/issues/1024 is released, this change can be reverted

--- data-pipeline/scripts/dependencies.R
+++ data-pipeline/scripts/dependencies.R
@@ -1,5 +1,7 @@
 requiredPackages <- c('tidyverse','EpiEstim', 'doParallel', 'foreach')
 chooseCRANmirror(ind=1, graphics=FALSE)
+install.packages('devtools')
+devtools::install_version("cpp11", version = "0.1", repos = "http://cran.us.r-project.org")
 for(p in requiredPackages){
   if(!require(p,character.only = TRUE)) install.packages(p)
   library(p,character.only = TRUE)
