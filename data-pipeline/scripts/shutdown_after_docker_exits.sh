bf4e8554afa07adb7dd775b8b3467183aaf84971
Pavel Janata <pavel@janata.me>
2020-08-27 14:23:32 +0200

    make sure the startup script won't block model

--- data-pipeline/scripts/shutdown_after_docker_exits.sh
+++ data-pipeline/scripts/shutdown_after_docker_exits.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# sleep first so that the docker container can start up
-sleep 300
+function check_docker_and_shutdown {
+  # sleep first so that the docker container can start up
+  sleep 300
 
-while true; do
-  if docker ps | grep -q npi-model; then
-    echo "NPI model is still running "
-    sleep 1
-  else
-    echo "No container running, deleting instance"
-    gcloud compute instances stop $(hostname) --zone \
-      $(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4) -q
-  fi
-done
+  while true; do
+    if docker ps | grep -q npi-model; then
+      echo "NPI model is still running "
+      sleep 1
+    else
+      echo "No container running, deleting instance"
+      gcloud compute instances stop $(hostname) --zone \
+        $(curl -H Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone -s | cut -d/ -f4) -q
+    fi
+  done
+}
+
+check_docker_and_shutdown &
