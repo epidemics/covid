f59150cf2a9b00c293027f6e9122fe370af48fbd
Marek Pukaj <marek.pukaj@gmail.com>
2020-09-01 10:04:24 +0200

    Change logscale conditions (#521)
    Co-authored-by: Marek Pukaj <Marek.Pukaj@blindspot.ai>

--- web/frontend/page_rt_estimates/NPIModelVisualizationUtils.tsx
+++ web/frontend/page_rt_estimates/NPIModelVisualizationUtils.tsx
@@ -79,7 +79,7 @@ const createAboveAndBelow = (
   ];
 };
 
-function isSameDay(d1: Date, d2: Date) {
+export function isSameDay(d1: Date, d2: Date) {
   return (
     d1.getFullYear() === d2.getFullYear() &&
     d1.getMonth() === d2.getMonth() &&
